5c321d2e60b4a89ea9a22bbddab4c57ff0b21575
AdityaSharmajr <149559271+AdityaSharmajr@users.noreply.github.com>
2023-11-03 06:50:07 +0530

    Add files via upload

---
+++ SafeTalk/src/main/java/com/SafeTalk/websocket/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.SafeTalk.websocket.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ SafeTalk/src/test/java/com/SafeTalk/websocket/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SafeTalk.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
